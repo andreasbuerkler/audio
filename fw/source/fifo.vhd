708701f3b0f3f9ce54e4269fc1047345bb2275b5
andreasbuerkler <andreas_buerkler@bluemail.ch>
2018-10-27 16:04:37 +0200

    FIFO updated

--- fw/source/fifo.vhd
+++ fw/source/fifo.vhd
@@ -3,6 +3,7 @@
 -- Date      : 20.10.2018
 -- Filename  : fifo.vhd
 -- Changelog : 20.10.2018 - file created
+--             27.10.2018 - reject functionality added
 --------------------------------------------------------------------------------
 
 library ieee;
@@ -13,7 +14,7 @@ entity fifo is
 generic (
     size_exp_g   : positive := 10;
     data_width_g : positive := 8;
-    use_reject_g : boolean  := false); -- TODO: add reject functionality
+    use_reject_g : boolean  := false);
 port (
     clk_i    : in  std_logic;
     reset_i  : in  std_logic;
@@ -33,18 +34,22 @@ architecture rtl of fifo is
 
     type mem_t is array(natural range <>) of std_logic_vector(data_width_g-1 downto 0);
 
-    signal mem_r              : mem_t((2**size_exp_g)-1 downto 0) := (others => (others => '0'));
-    signal rd_data_r          : std_logic_vector(data_width_g-1 downto 0) := (others => '0');
-    signal preload_r          : std_logic := '0';
+    signal mem_r                    : mem_t((2**size_exp_g)-1 downto 0) := (others => (others => '0'));
+    signal rd_data_r                : std_logic_vector(data_width_g-1 downto 0) := (others => '0');
+    signal preload_r                : std_logic := '0';
 
-    signal wr_addr_r          : std_logic_vector(size_exp_g-1 downto 0) := (others => '0');
-    signal rd_addr_r          : std_logic_vector(size_exp_g-1 downto 0) := (others => '0');
-    signal full_next          : std_logic;
-    signal full_r             : std_logic := '0';
-    signal empty_next         : std_logic;
-    signal empty_r            : std_logic := '1';
-    signal word_counter_r     : unsigned(size_exp_g downto 0) := (others => '0');
-    signal word_counter_inc_r : std_logic := '0';
+    signal wr_addr_r                : std_logic_vector(size_exp_g-1 downto 0) := (others => '0');
+    signal wr_addr_store_r          : std_logic_vector(size_exp_g-1 downto 0) := (others => '0');
+    signal rd_addr_r                : std_logic_vector(size_exp_g-1 downto 0) := (others => '0');
+    signal full_next                : std_logic;
+    signal full_r                   : std_logic := '0';
+    signal empty_next               : std_logic;
+    signal empty_r                  : std_logic := '1';
+    signal word_counter_r           : unsigned(size_exp_g downto 0) := (others => '0');
+    signal word_counter_next_r      : unsigned(size_exp_g downto 0) := (others => '0');
+    signal word_counter_inc_r       : std_logic := '0';
+    signal store_delay_r            : std_logic := '0';
+    signal reject_delay_r           : std_logic := '0';
 
     attribute ramstyle : string;
     attribute ramstyle of mem_r : signal is "M9K";
@@ -69,13 +74,37 @@ begin
         end if;
     end process ram_rd_proc;
 
+    delay_proc : process (clk_i)
+        variable rd_en_v : std_logic := '0';
+        variable wr_en_v : std_logic := '0';
+    begin
+        if (rising_edge(clk_i)) then
+            rd_en_v := rd_i and (not empty_r);
+            wr_en_v := wr_i and (not full_r);
+            word_counter_inc_r <= wr_en_v;
+            if (use_reject_g) then
+                store_delay_r <= store_i;
+                reject_delay_r <= reject_i;
+            end if;
+        end if;
+    end process delay_proc;
+
     preload_proc : process (clk_i)
     begin
         if (rising_edge(clk_i)) then
-            if ((word_counter_r = to_unsigned(0, word_counter_r'length)) and (word_counter_inc_r = '1')) then
-                preload_r <= word_counter_inc_r;
+            if (use_reject_g) then
+                if ((word_counter_r = to_unsigned(0, word_counter_r'length)) and
+                    ((word_counter_next_r /= to_unsigned(0, word_counter_next_r'length)) or (word_counter_inc_r = '1'))) then
+                    preload_r <= store_delay_r and (not reject_delay_r);
+                else
+                    preload_r <= '0';
+                end if;
             else
-                preload_r <= '0';
+                if (word_counter_r = to_unsigned(0, word_counter_r'length)) then
+                    preload_r <= word_counter_inc_r;
+                else
+                    preload_r <= '0';
+                end if;
             end if;
         end if;
     end process preload_proc;
@@ -88,7 +117,26 @@ begin
             rd_en_v := rd_i and (not empty_r);
             wr_en_v := wr_i and (not full_r);
             if (wr_en_v = '1') then
-                wr_addr_r <= std_logic_vector(unsigned(wr_addr_r) + 1);
+                if (use_reject_g) then
+                    if (reject_i = '1') then
+                        wr_addr_r <= wr_addr_store_r;
+                    else
+                        if (store_i = '1') then
+                            wr_addr_store_r <= std_logic_vector(unsigned(wr_addr_r) + 1);
+                        end if;
+                        wr_addr_r <= std_logic_vector(unsigned(wr_addr_r) + 1);
+                    end if;
+                else
+                    wr_addr_r <= std_logic_vector(unsigned(wr_addr_r) + 1);
+                end if;
+            else
+                if (use_reject_g) then
+                   if (reject_i = '1') then
+                        wr_addr_r <= wr_addr_store_r;
+                    elsif (store_i = '1') then
+                        wr_addr_store_r <= wr_addr_r;
+                    end if;
+                end if;
             end if;
             if (((rd_en_v = '1') and (empty_next = '0')) or (preload_r = '1')) then
                 rd_addr_r <= std_logic_vector(unsigned(rd_addr_r) + 1);
@@ -97,57 +145,117 @@ begin
     end process addr_proc;
 
     counter_proc : process (clk_i)
+    begin
+        if (rising_edge(clk_i)) then
+            if ((rd_i = '0') or (empty_r = '1')) then
+                if (use_reject_g) then
+                    if (word_counter_inc_r = '1') then
+                        if (reject_delay_r = '1') then
+                            word_counter_next_r <= word_counter_r;
+                        elsif (store_delay_r = '1') then
+                            word_counter_next_r <= word_counter_next_r + 1;
+                            word_counter_r <= word_counter_next_r + 1;
+                        else
+                            word_counter_next_r <= word_counter_next_r + 1;
+                        end if;
+                    else
+                        if (reject_delay_r = '1') then
+                            word_counter_next_r <= word_counter_r;
+                        elsif (store_delay_r = '1') then
+                            word_counter_r <= word_counter_next_r;
+                        end if;
+                    end if;
+                else
+                    if (word_counter_inc_r = '1') then
+                        word_counter_r <= word_counter_r + 1;
+                    end if;
+                end if;
+            else
+                if (use_reject_g) then
+                    if (reject_delay_r = '1') then
+                        word_counter_next_r <= word_counter_r - 1;
+                        word_counter_r <= word_counter_r - 1;
+                    elsif (store_delay_r = '1') then
+                        if (word_counter_inc_r = '0') then
+                            word_counter_next_r <= word_counter_next_r - 1;
+                            word_counter_r <= word_counter_next_r - 1;
+                        else
+                            word_counter_r <= word_counter_next_r;
+                        end if;
+                    else
+                        if (word_counter_inc_r = '0') then
+                            word_counter_next_r <= word_counter_next_r - 1;
+                        end if;
+                        word_counter_r <= word_counter_r - 1;
+                    end if;
+                else
+                    if (word_counter_inc_r = '0') then
+                        word_counter_r <= word_counter_r - 1;
+                    end if;
+                end if;
+            end if;
+        end if;
+    end process counter_proc;
+
+    flag_async : process (word_counter_inc_r, word_counter_r, word_counter_next_r, store_delay_r, reject_delay_r)
+    begin
+        if (use_reject_g) then
+            if (word_counter_r = to_unsigned(1, word_counter_r'length)) then
+                if (((word_counter_next_r /= to_unsigned(1, word_counter_next_r'length)) or (word_counter_inc_r = '1')) and (store_delay_r = '1') and (reject_delay_r = '0')) then
+                    empty_next <= '0';
+                else
+                    empty_next <= '1';
+                end if;
+            else
+                empty_next <= '0';
+            end if;
+        else
+            if ((word_counter_inc_r = '0') and (word_counter_r = to_unsigned(1, word_counter_r'length))) then
+                empty_next <= '1';
+            else
+                empty_next <= '0';
+            end if;
+        end if;
+
+        if (use_reject_g) then
+            if (word_counter_next_r(word_counter_next_r'high downto 1) = to_unsigned((2**(size_exp_g-1))-1, word_counter_next_r'length-1)) then
+                if (word_counter_next_r(0) = '0') then
+                    full_next <= word_counter_inc_r;
+                else
+                    full_next <= '1';
+                end if;
+            else
+                full_next <= word_counter_next_r(word_counter_next_r'high);
+            end if;
+        else
+            if (word_counter_r(word_counter_r'high downto 1) = to_unsigned((2**(size_exp_g-1))-1, word_counter_r'length-1)) then
+                if (word_counter_r(0) = '0') then
+                    full_next <= word_counter_inc_r;
+                else
+                    full_next <= '1';
+                end if;
+            else
+                full_next <= '0';
+            end if;
+        end if;
+    end process flag_async;
+
+    flag_proc : process (clk_i)
         variable rd_en_v : std_logic := '0';
         variable wr_en_v : std_logic := '0';
     begin
         if (rising_edge(clk_i)) then
             rd_en_v := rd_i and (not empty_r);
             wr_en_v := wr_i and (not full_r);
-            word_counter_inc_r <= wr_en_v;
-            if (rd_en_v = '0') then
-                if (word_counter_inc_r = '1') then
-                    word_counter_r <= word_counter_r + 1;
-                end if;
-            else
-                if (word_counter_inc_r = '0') then
-                    word_counter_r <= word_counter_r - 1;
-                end if;
-            end if;
-        end if;
-    end process counter_proc;
 
-    flag_async : process (rd_i, empty_r, word_counter_inc_r, word_counter_r)
-        variable rd_en_v : std_logic := '0';
-        variable wr_en_v : std_logic := '0';
-    begin
-        rd_en_v := rd_i and (not empty_r);
-        wr_en_v := wr_i and (not full_r);
-        if ((rd_en_v = '1') and (word_counter_inc_r = '0') and (word_counter_r = to_unsigned(1, word_counter_r'length))) then
-            empty_next <= '1';
-        else
-            empty_next <= '0';
-        end if;
-
-        if ((word_counter_r(word_counter_r'high downto 1) = to_unsigned((2**(size_exp_g-1))-1, word_counter_r'length-1)) and (wr_en_v = '1') and (rd_en_v = '0')) then
-            if (word_counter_r(0) = '0') then
-                full_next <= word_counter_inc_r;
-            else
-                full_next <= '1';
-            end if;
-        end if;
-    end process flag_async;
-
-    flag_proc : process (clk_i)
-    begin
-        if (rising_edge(clk_i)) then
             if (preload_r = '1') then
                 empty_r <= '0';
-            elsif (empty_next = '1') then
+            elsif ((empty_next = '1') and (rd_en_v = '1')) then
                 empty_r <= '1';
             end if;
-            if ((rd_i = '1') and (full_r = '1')) then
+            if (((rd_i = '1') and (full_r = '1') and (empty_r = '0')) or ((reject_i = '1') and (full_next = '0'))) then
                 full_r <= '0';
-            elsif (full_next = '1') then
+            elsif ((full_next = '1') and (wr_en_v = '1') and (rd_en_v = '0')) then
                 full_r <= '1';
             end if;
         end if;
--- fw/testbench/fifo_tb.vhd
+++ fw/testbench/fifo_tb.vhd
@@ -35,14 +35,29 @@ architecture rtl of fifo_tb is
         empty_o  : out std_logic);
     end component fifo;
 
-    signal clk             : std_logic;
-    signal write_data_r    : std_logic_vector(15 downto 0) := (others => '0');
-    signal write_en_r      : std_logic := '0';
-    signal full            : std_logic;
-    signal read_data       : std_logic_vector(15 downto 0);
-    signal read_en_r       : std_logic := '0';
-    signal empty           : std_logic;
-    signal expected_data_r : std_logic_vector(15 downto 0) := (others => '0');
+    type std_logic_array_16_t is array (natural range <>) of std_logic_vector(15 downto 0);
+
+    signal clk                  : std_logic;
+    signal store_r              : std_logic := '0';
+    signal reject_r             : std_logic := '0';
+
+    signal write_data_r         : std_logic_vector(15 downto 0) := (others => '0');
+    signal write_en_r           : std_logic := '0';
+    signal full                 : std_logic;
+    signal read_data            : std_logic_vector(15 downto 0);
+    signal read_en_r            : std_logic := '0';
+    signal empty                : std_logic;
+    signal expected_data_r      : std_logic_vector(15 downto 0) := (others => '0');
+
+    signal write_data_2_r       : std_logic_vector(15 downto 0) := (others => '0');
+    signal write_en_2_r         : std_logic := '0';
+    signal full_2               : std_logic;
+    signal read_data_2          : std_logic_vector(15 downto 0);
+    signal read_en_2_r          : std_logic := '0';
+    signal empty_2              : std_logic;
+
+    signal expected_data_count_r     : integer := 0;   
+    signal expected_data_vec_count_r : integer := 0;
 
 begin
 
@@ -77,7 +92,7 @@ begin
     write_proc : process (clk)
         variable seed1_v : positive := 125;
         variable seed2_v : positive := 53;
-        variable rand_v  : real;   
+        variable rand_v  : real;
     begin
         if (rising_edge(clk)) then
             uniform(seed1_v, seed2_v, rand_v);
@@ -95,7 +110,7 @@ begin
     read_proc : process (clk)
         variable seed1_v : positive := 23;
         variable seed2_v : positive := 78;
-        variable rand_v  : real;   
+        variable rand_v  : real;
     begin
         if (rising_edge(clk)) then
             uniform(seed1_v, seed2_v, rand_v);
@@ -112,4 +127,94 @@ begin
         end if;
     end process read_proc;
 
+--------------------------------------------------------------------
+
+    i_fifo_2 : fifo
+    generic map (
+        size_exp_g   => 4,
+        data_width_g => 16,
+        use_reject_g => true)
+    port map (
+        clk_i    => clk,
+        reset_i  => '0',
+        -- write port
+        data_i   => write_data_2_r,
+        wr_i     => write_en_2_r,
+        store_i  => store_r,
+        reject_i => reject_r,
+        full_o   => full_2,
+        -- read port
+        data_o   => read_data_2,
+        rd_i     => read_en_2_r,
+        empty_o  => empty_2);
+
+    stimuli_proc : process (clk)
+        variable seed1_v : positive := 254;
+        variable seed2_v : positive := 14;
+        variable rand_v  : real;
+    begin
+        if (rising_edge(clk)) then
+            uniform(seed1_v, seed2_v, rand_v);
+            if (rand_v > 0.5) then
+                write_en_2_r <= '1';
+            else
+                write_en_2_r <= '0';
+            end if;
+            uniform(seed1_v, seed2_v, rand_v);
+            if (rand_v > 0.5) then
+                read_en_2_r <= '1';
+            else
+                read_en_2_r <= '0';
+            end if;
+            uniform(seed1_v, seed2_v, rand_v);
+            if (rand_v > 0.9) then
+                store_r <= '1';
+            else
+                store_r <= '0';
+            end if;
+            uniform(seed1_v, seed2_v, rand_v);
+            if (rand_v > 0.9) then
+                reject_r <= '1';
+            else
+                reject_r <= '0';
+            end if;
+            if (full_2 = '0') and (write_en_2_r = '1') then
+                write_data_2_r <= std_logic_vector(unsigned(write_data_2_r) + 1);
+            end if;
+        end if;
+    end process stimuli_proc;
+
+    model_proc : process (clk)
+        variable expected_data_2_v         : std_logic_array_16_t(2**5 downto 0) := (others => (others => '0'));
+        variable expected_data_vec_v       : std_logic_array_16_t(2**5 downto 0) := (others => (others => '0'));
+        variable expected_data_count_v     : integer := 0;   
+        variable expected_data_vec_count_v : integer := 0;
+    begin
+        if (rising_edge(clk)) then
+            if ((write_en_2_r = '1') and (full_2 = '0')) then
+                expected_data_vec_v := expected_data_vec_v(expected_data_vec_v'high-1 downto 0) & write_data_2_r;
+                expected_data_vec_count_v := expected_data_vec_count_v + 1;
+            end if;
+            if (reject_r = '1') then
+                expected_data_vec_count_v := 0;
+            elsif (store_r = '1') then
+                for i in 0 to expected_data_vec_count_v-1 loop
+                    expected_data_2_v(expected_data_count_v) := expected_data_vec_v(expected_data_vec_count_v-1-i);
+                    expected_data_count_v := expected_data_count_v + 1;
+                end loop;
+                expected_data_vec_count_v := 0;
+            end if;
+
+            if ((read_en_2_r = '1') and (empty_2 = '0')) then
+                assert (read_data_2 = expected_data_2_v(0)) report "error 2: expected " & integer'image(to_integer(unsigned(expected_data_2_v(0)))) &
+                                                                ", got " & integer'image(to_integer(unsigned(read_data_2))) severity error;
+                expected_data_2_v := x"0000" & expected_data_2_v(expected_data_2_v'high downto 1);
+                expected_data_count_v := expected_data_count_v - 1;
+            end if;
+
+            expected_data_count_r <= expected_data_count_v;
+            expected_data_vec_count_r <= expected_data_vec_count_v;
+
+        end if;
+    end process model_proc;
 end rtl;
