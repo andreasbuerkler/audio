d6299c0f55e89fae4c3fa495ce9e082e83a5313c
Andreas Buerkler <buerkler@gmail.com>
2020-04-14 17:48:55 +0200

    hyper ram controller updated

--- fw/source/hyper_ram_controller.vhd
+++ fw/source/hyper_ram_controller.vhd
@@ -60,7 +60,6 @@ architecture rtl of hyper_ram_controller is
     signal init_counter_r      : unsigned(log2ceil(power_up_wait_cycles_c+1)-1 downto 0) := to_unsigned(power_up_wait_cycles_c, log2ceil(power_up_wait_cycles_c+1));
     signal data_r              : std_logic_vector(data_width_g-1 downto 0) := (others => '0');
     signal data_read_r         : std_logic_vector(data_width_g-1 downto 0) := (others => '0');
-    signal data_read_cc_r      : std_logic_vector(data_width_g-1 downto 0) := (others => '0');
     signal data_ack_r          : std_logic := '0';
     signal data_halfword_sel_r : std_logic := '0';
     signal burst_counter_r     : unsigned(log2ceil(max_burst_size_g)-1 downto 0) := (others => '0');
@@ -82,16 +81,14 @@ architecture rtl of hyper_ram_controller is
     signal hyper_data_en_r       : std_logic := '0';
     signal hyper_data_en_delay_r : std_logic := '0';
     signal hyper_data_in         : std_logic_vector(15 downto 0);
-    signal hyper_data_in_upper_r : std_logic_vector(7 downto 0) := (others => '0');
     signal hyper_data_out_r      : std_logic_vector(15 downto 0) := (others => '0');
 
     signal hyper_rwds_en_r       : std_logic := '0';
     signal hyper_rwds_en_delay_r : std_logic := '0';
+    signal hyper_rwds_n          : std_logic;
 
     signal hyper_data_in_counter_r       : unsigned(log2ceil(2*max_burst_size_g) downto 0) := (others => '0');
     signal hyper_data_in_counter_cc_r    : unsigned(log2ceil(2*max_burst_size_g) downto 0) := (others => '0');
-    signal hyper_data_in_counter_delay_r : unsigned(log2ceil(2*max_burst_size_g) downto 0) := (others => '0');
-    signal hyper_data_in_counter_low_r   : std_logic := '0';
     signal hyper_data_in_counter_reset_r : std_logic := '0';
 
 begin
@@ -138,7 +135,6 @@ begin
                     end if;
 
                 when start_s =>
-                    hyper_data_in_counter_reset_r <= '1';
                     ca_r(ca_read_sel_c) <= read_request_r;
                     ca_r(ca_space_sel_c) <= '0'; -- access to memory space
                     ca_r(ca_burst_sel_c) <= '1'; -- linear burst
@@ -189,6 +185,7 @@ begin
 
                 when wait_s =>
                     hyper_data_en_r <= '0';
+                    hyper_data_in_counter_reset_r <= '1';
                     -- wait until initial latency has passed
                     if (vector_or(std_logic_vector(latency_counter_r)) = '0') then
                         if (ca_r(ca_read_sel_c) = '0') then
@@ -203,13 +200,13 @@ begin
 
                 when read_s =>
                     hyper_data_en_r <= '0';
-                    if (hyper_data_in_counter_low_r = '1') and (hyper_data_in_counter_delay_r(0) = '0') then
+                    if (hyper_data_in_counter_cc_r(0) = '1') and (hyper_data_in_counter_r(0) = '0') then
+                        if (hyper_data_in_counter_cc_r(hyper_data_in_counter_cc_r'high downto 1) = unsigned(burst_size_r)) then
+                            hyper_cs_en_r <= '0';
+                            hyper_clk_en_r <= '0';
+                            fsm_r <= idle_s;
+                        end if;
                         data_ack_r <= '1';
-                    end if;
-                    if (hyper_data_in_counter_delay_r(hyper_data_in_counter_delay_r'high downto 1) = (unsigned(burst_size_r)+1)) then
-                        hyper_cs_en_r <= '0';
-                        hyper_clk_en_r <= '0';
-                        fsm_r <= idle_s;
                     end if;
 
                 when write_s =>
@@ -256,23 +253,23 @@ begin
             hyper_cs_en_delay_r <= hyper_cs_en_r;
             hyper_data_en_delay_r <= hyper_data_en_r;
             hyper_rwds_en_delay_r <= hyper_rwds_en_r;
-            data_read_cc_r <= data_read_r;
         end if;
     end process delay_proc;
 
     data_read_proc : process (clk_i)
     begin
-        if (falling_edge(clk_i)) then
-            -- input data reordering on falling edge to improve timing
-            hyper_data_in_upper_r <= hyper_data_in(7 downto 0);
-            if (hyper_data_in_counter_cc_r(0) = '0') and (hyper_data_in_counter_delay_r(0) = '1') then
-                data_read_r(31 downto 16) <= hyper_data_in(15 downto 8) & hyper_data_in_upper_r;
-            elsif (hyper_data_in_counter_cc_r(0) = '1') and (hyper_data_in_counter_delay_r(0) = '0') then
-                data_read_r(15 downto 0) <= hyper_data_in(15 downto 8) & hyper_data_in_upper_r;
+        if (rising_edge(clk_i)) then
+            -- input data reordering
+            if (hyper_data_in_counter_r(0) = '0') and (hyper_data_in_counter_cc_r(0) = '1') then
+                data_read_r(31 downto 16) <= hyper_data_in(7 downto 0) & hyper_data_in(15 downto 8);
+            elsif (hyper_data_in_counter_r(0) = '1') and (hyper_data_in_counter_cc_r(0) = '0') then
+                data_read_r(15 downto 0) <= hyper_data_in(7 downto 0) & hyper_data_in(15 downto 8);
             end if;
-            hyper_data_in_counter_cc_r <= hyper_data_in_counter_r;
-            hyper_data_in_counter_delay_r <= hyper_data_in_counter_cc_r;
-            hyper_data_in_counter_low_r <= hyper_data_in_counter_delay_r(0);
+            if (hyper_data_in_counter_reset_r = '1') then
+                hyper_data_in_counter_cc_r <= (others => '0');
+            else
+                hyper_data_in_counter_cc_r <= hyper_data_in_counter_r;
+            end if;
         end if;
     end process data_read_proc;
 
@@ -280,10 +277,12 @@ begin
     begin
         if (hyper_data_in_counter_reset_r = '1') then
             hyper_data_in_counter_r <= (others => '0');
-        elsif (rising_edge(hyper_rwds_io)) then
+        elsif (falling_edge(hyper_rwds_io)) then
             hyper_data_in_counter_r <= hyper_data_in_counter_r + 1;
         end if;
     end process data_in_counter_proc;
+
+    hyper_rwds_n <= not hyper_rwds_io;
 
     i_ddr_data_reg : altddio_bidir
     generic map (
@@ -299,7 +298,7 @@ begin
     port map (
         datain_h   => hyper_data_out_r(15 downto 8),
         datain_l   => hyper_data_out_r(7 downto 0),
-        inclock    => hyper_rwds_io,
+        inclock    => hyper_rwds_n,
         outclock   => clk_i,
         dataout_h  => hyper_data_in(7 downto 0),
         dataout_l  => hyper_data_in(15 downto 8),
@@ -328,7 +327,7 @@ begin
 
     hyper_rst_n_o <= not reset_i;
 
-    ctrl_data_o <= data_read_cc_r;
+    ctrl_data_o <= data_read_r;
     ctrl_ack_o <= data_ack_r;
 
 end rtl;
